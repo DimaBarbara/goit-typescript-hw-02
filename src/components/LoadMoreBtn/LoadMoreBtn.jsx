1a962363dbc8287db3bfec69b33e371bec3e2501
Dima <oxxTerm0s@gmail.com>
2025-03-10 20:14:31 +0200

    add TS

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import styles from "./LoadMoreBtn.module.css"
-const LoadMoreBtn = ({loadMore}) => {
-  return (
-    <button className={styles.button} type="button" onClick={loadMore}>Load more</button>
-  )
-}
-
-export default LoadMoreBtn
